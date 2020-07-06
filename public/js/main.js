e8d14a9fd7eaf85d840d1da1654cfab252029ebd
ashish136 <ashish136sharma@gmail.com>
2020-07-07 01:37:05 +0530

    JSON PRTTIFIER:added replacer

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,25 @@
 let inputEle = document.getElementById("idInputBox");
 let outputEle = document.getElementById("idOutputBox");
+var filterArr = [],dataTypeArr=[];
+const fnReplacer = function(key, value) {
+    if(key=="")return value;
+
+    let returnValue=value;
+    if(filterArr.length && !filterArr.includes(key))returnValue=undefined;
+    if(dataTypeArr.length && dataTypeArr.includes(typeof value))returnValue=undefined;
+    
+    return returnValue;
+}
+
 const fnPrettify = function() {
     try {
         let inputJson = JSON.parse(inputEle.value);
-        outputEle.innerHTML = `${JSON.stringify(inputJson, undefined, 4)}`;
+        outputEle.innerHTML = `${JSON.stringify(inputJson, fnReplacer, 4)}`;
     } catch (e) {
         outputEle.innerHTML = "Invalid json";
     }
 }
+
 document.getElementById("idInputBox").onkeyup = function(e) {
     var evt = e ? e : event;
     if ((evt.keyCode && evt.keyCode != 13) || evt.which != 13)
