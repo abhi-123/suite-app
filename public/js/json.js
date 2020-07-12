6275044372eea305b8f4717bf38744bb339b447f
ashish136 <ashish136sharma@gmail.com>
2020-07-12 23:39:08 +0530

    JSON FORMATTER: added filter functions

--- public/js/json.js
+++ public/js/json.js
@@ -2,6 +2,9 @@ const inputEle = document.getElementById("idInputBox");
 const outputEle = document.getElementById("idOutputBox");
 const verticalBar = document.getElementById("vertical-bar");
 const selectSpace = document.getElementById("IdSpaces");
+const propEle = document.getElementById("multi-input");
+const selectDataType = document.getElementById("id-data-type-filter");
+
 let filterArr = [],dataTypeArr=[],spaces=4;
 
 const fnReplacer = function(key, value) {
@@ -9,22 +12,39 @@ const fnReplacer = function(key, value) {
 
     let returnValue=value;
     if(filterArr.length && !filterArr.includes(key))returnValue=undefined;
-    if(dataTypeArr.length && dataTypeArr.includes(typeof value))returnValue=undefined;
+    if(dataTypeArr.length && !dataTypeArr.includes(typeof value))returnValue=undefined;
     
     return returnValue;
 }
 
-selectSpace.onchange = function (e) {
-    spaces = this.options[this.selectedIndex].value;
-    spaces = parseInt(spaces);
+const setFilterProp = function(){
+    let propStr = propEle.value;
+    if(propStr.trim().length>0)
+        filterArr = propStr.trim().split(",");
+    else filterArr = [];
+
+    if(selectSpace.selectedIndex)
+        spaces = parseInt(selectSpace.options[selectSpace.selectedIndex].value);
+    else spaces = 4;
+
+    let dataTypes = document.getElementsByName("dataType");
+    dataTypeArr=[];
+    dataTypes.forEach(type=>{
+        if(type.checked)dataTypeArr.push(type.value);
+    })
 }
 
+// selectSpace.onchange = function (e) {
+//     spaces = this.options[this.selectedIndex].value;
+//     spaces = parseInt(spaces);
+// }
+
 const fnPrettify = function() {
+    setFilterProp();
     try {
         let inputJson = JSON.parse(inputEle.value);
         outputEle.innerHTML = `${JSON.stringify(inputJson, fnReplacer, spaces)}`;
         addLines(outputEle.innerHTML);
-
     } catch (e) {
         outputEle.innerHTML = "Invalid json";
     }
