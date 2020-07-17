7ddcf674320ede97e7a2a11996f0c15bd7239fcb
Anmol Agarwal <anmol.ag123@gmail.com>
2020-07-18 01:33:31 +0530

    Initial code for displaying Matched Text and capturing groups

--- public/js/regex.js
+++ public/js/regex.js
@@ -1,13 +1,47 @@
 
 document.getElementById("regex-validate").addEventListener("click", function() {
 
-    const reg = new RegExp(document.getElementById("regex-exp-input").value);
-
+    const reg = new RegExp(document.getElementById("regex-exp-input").value, 'g');
     const str = document.getElementById("regex-string-input").value;
+    let text;
     let results = reg.test(str);
 
     if (reg === "" || str === "") {
         results = false;
+    }
+
+    let regResults;
+    let obj = [];
+    while( (regResults = reg.exec(str)) !== null ) {        
+        let index = regResults.index;
+        let text = regResults[0];
+
+        let capturedGroups 
+                = regResults.slice(1, regResults.length)
+                                .map((arr) => {
+                                    return arr;
+                                });
+        obj.push({
+            index,
+            text,
+            capturedGroups
+        });                        
+    }
+
+    let initialIndex = 0;
+    let div = "";
+
+    //Displaying Matched Text and Captured Groups
+    for(let i = 0;i< obj.length; i++) {
+
+        div = document.createElement("div");
+        div.innerHTML = `<div><b>Matched Text :</b> ${obj[i].text}</div>`
+        for(let j=0;j<obj[i].capturedGroups.length;j++) {
+            let tempDiv = document.createElement("div");
+            tempDiv.innerHTML = `<div><b>Captured Group ${j+1}:</b>  ${obj[i].capturedGroups[j]}</div>`;
+            div.appendChild(tempDiv);
+        }
+        document.getElementById("regex-results").innerHTML = div.innerHTML;
     }
 
     document.getElementById("valid-text").classList.remove("display-none");
